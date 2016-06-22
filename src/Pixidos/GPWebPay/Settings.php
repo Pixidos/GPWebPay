4c6dc4bfdd34c68a0f2be5de4df82dbb6f934507
Ondra Votava <ja@ondravotava.cz>
2016-06-22 14:41:28 +0200

    reformat code

--- src/Pixidos/GPWebPay/Settings.php
+++ src/Pixidos/GPWebPay/Settings.php
@@ -16,141 +16,144 @@ namespace Pixidos\GPWebPay;
 
 class Settings
 {
-    /** @var string */
-    private $privateKey;
+	/** @var string */
+	private $privateKey;
 
-    /** @var string */
-    private $privateKeyPassword;
+	/** @var string */
+	private $privateKeyPassword;
 
-    /** @var string */
-    private $publicKey;
+	/** @var string */
+	private $publicKey;
 
-    /**
-     * @var  string $url
-     */
-    private $url;
-    /**
-     * @var  string $merchantNumber
-     */
-    private $merchantNumber;
+	/**
+	 * @var  string $url
+	 */
+	private $url;
+	/**
+	 * @var  string $merchantNumber
+	 */
+	private $merchantNumber;
 
-    /**
-     * @var int $depositFlag
-     */
-    private $depositFlag;
+	/**
+	 * @var int $depositFlag
+	 */
+	private $depositFlag;
 
-    private $defaultGatewayKey;
+	private $defaultGatewayKey;
 
-    /**
-     * Settings constructor.
-     * @param string $privateKey
-     * @param string $privateKeyPassword
-     * @param string $publicKey
-     * @param string $url
-     * @param string $merchantNumber
-     * @param int $depositFlag
-     * @param string $gatewayKey
-     */
-    public function __construct($privateKey,
-                                $privateKeyPassword,
-                                $publicKey,
-                                $url,
-                                $merchantNumber,
-                                $depositFlag,
-                                $gatewayKey)
-    {
-        if(! is_array($privateKey)){
-            $key = $privateKey;
-            $privateKey = [];
-            $privateKey[$gatewayKey] = $key;
-        }
-        $this->privateKey = $privateKey;
+	/**
+	 * Settings constructor.
+	 * @param string $privateKey
+	 * @param string $privateKeyPassword
+	 * @param string $publicKey
+	 * @param string $url
+	 * @param string $merchantNumber
+	 * @param int $depositFlag
+	 * @param string $gatewayKey
+	 */
+	public function __construct(
+		$privateKey,
+		$privateKeyPassword,
+		$publicKey,
+		$url,
+		$merchantNumber,
+		$depositFlag,
+		$gatewayKey
+	) {
+		if (! is_array($privateKey)) {
+			$key = $privateKey;
+			$privateKey = [];
+			$privateKey[$gatewayKey] = $key;
+		}
 
-        if(! is_array($privateKeyPassword)){
-            $pwd = $privateKeyPassword;
-            $privateKeyPassword = [];
-            $privateKeyPassword[$gatewayKey] = $pwd;
-        }
-        $this->privateKeyPassword = $privateKeyPassword;
+		$this->privateKey = $privateKey;
 
-        $this->publicKey = $publicKey;
-        $this->url = $url;
+		if (! is_array($privateKeyPassword)) {
+			$pwd = $privateKeyPassword;
+			$privateKeyPassword = [];
+			$privateKeyPassword[$gatewayKey] = $pwd;
+		}
 
-        if(! is_array($merchantNumber)){
-            $merchant = $merchantNumber;
-            $merchantNumber = [];
-            $merchantNumber[$gatewayKey] = $merchant;
-        }
+		$this->privateKeyPassword = $privateKeyPassword;
 
-        $this->merchantNumber = $merchantNumber;
-        $this->depositFlag = $depositFlag;
-        $this->defaultGatewayKey = $gatewayKey;
-    }
+		$this->publicKey = $publicKey;
+		$this->url = $url;
+
+		if (! is_array($merchantNumber)) {
+			$merchant = $merchantNumber;
+			$merchantNumber = [];
+			$merchantNumber[$gatewayKey] = $merchant;
+		}
+
+		$this->merchantNumber = $merchantNumber;
+		$this->depositFlag = $depositFlag;
+		$this->defaultGatewayKey = $gatewayKey;
+	}
 
 
-    /**
-     * @return string
-     */
-    public function getUrl()
-    {
-        return $this->url;
-    }
+	/**
+	 * @return string
+	 */
+	public function getUrl()
+	{
+		return $this->url;
+	}
 
-    /**
-     * @param null $gatewayKey
-     * @return string
-     */
-    public function getMerchantNumber($gatewayKey = NULL)
-    {
-        if(is_null($gatewayKey)){
-            $gatewayKey = $this->getDefaultGatewayKey(); //czk is config default
-        }
-        return $this->merchantNumber[$gatewayKey];
-    }
+	/**
+	 * @param null $gatewayKey
+	 * @return string
+	 */
+	public function getMerchantNumber($gatewayKey = NULL)
+	{
+		if (is_null($gatewayKey)) {
+			$gatewayKey = $this->getDefaultGatewayKey(); //czk config default
+		}
+		return $this->merchantNumber[$gatewayKey];
+	}
 
-    /**
-     * @param null $gatewayKey
-     * @return string
-     */
-    public function getPrivateKey($gatewayKey = NULL)
-    {
-        if(is_null($gatewayKey)){
-            $gatewayKey = $this->getDefaultGatewayKey(); //czk is config default
-        }
-        return $this->privateKey[$gatewayKey];
-    }
+	/**
+	 * @param null $gatewayKey
+	 * @return string
+	 */
+	public function getPrivateKey($gatewayKey = NULL)
+	{
+		if (is_null($gatewayKey)) {
+			$gatewayKey = $this->getDefaultGatewayKey(); //czk config default
+		}
+		return $this->privateKey[$gatewayKey];
+	}
 
-    /**
-     * @return string
-     */
-    public function getPublicKey()
-    {
-        return $this->publicKey;
-    }
+	/**
+	 * @return string
+	 */
+	public function getPublicKey()
+	{
+		return $this->publicKey;
+	}
 
-    /**
-     * @param null $gatewayKey
-     * @return string
-     */
-    public function getPrivateKeyPassword($gatewayKey = NULL)
-    {
-        if(is_null($gatewayKey)){
-            $gatewayKey = $this->getDefaultGatewayKey(); //czk is config default
-        }
-        return $this->privateKeyPassword[$gatewayKey];
-    }
+	/**
+	 * @param null $gatewayKey
+	 * @return string
+	 */
+	public function getPrivateKeyPassword($gatewayKey = NULL)
+	{
+		if (is_null($gatewayKey)) {
+			$gatewayKey = $this->getDefaultGatewayKey(); //czk config default
+		}
+		return $this->privateKeyPassword[$gatewayKey];
+	}
 
-    /**
-     * @return int
-     */
-    public function getDepositFlag()
-    {
-        return $this->depositFlag;
-    }
+	/**
+	 * @return int
+	 */
+	public function getDepositFlag()
+	{
+		return $this->depositFlag;
+	}
 
-    public function getDefaultGatewayKey()
-    {
-        return $this->defaultGatewayKey;
-    }
+	public function getDefaultGatewayKey()
+	{
+		return $this->defaultGatewayKey;
+	}
 
 }
