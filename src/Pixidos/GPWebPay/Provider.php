b781676e64f1e59bc33fffbc2b04f5d8f1c7066f
Ondra Votava <ja@ondravotava.cz>
2016-06-22 14:43:16 +0200

    reformat code

--- src/Pixidos/GPWebPay/Provider.php
+++ src/Pixidos/GPWebPay/Provider.php
@@ -20,126 +20,133 @@ use Pixidos\GPWebPay\Exceptions\GPWebPayException;
 class Provider
 {
 
-    /**
-     * @var Settings $settings
-     */
-    private $settings;
+	/**
+	 * @var Settings $settings
+	 */
+	private $settings;
 
-    /**
-     * @var  Request $request
-     */
-    private $request;
+	/**
+	 * @var  Request $request
+	 */
+	private $request;
 
-    /**
-     * @var  Signer $signer
-     */
-    private $signer;
+	/**
+	 * @var  Signer $signer
+	 */
+	private $signer;
 
-    /**
-     * Provider constructor.
-     * @param Settings $settings
-     */
-    public function __construct(Settings $settings)
-    {
-        $this->settings = $settings;
-    }
+	/**
+	 * Provider constructor.
+	 * @param Settings $settings
+	 */
+	public function __construct(Settings $settings)
+	{
+		$this->settings = $settings;
+	}
 
 
-    /**
-     * @param Operation $operation
-     * @return $this
-     */
-    public function createRequest(Operation $operation)
-    {
-        $this->request = new Request($operation, $this->settings->getMerchantNumber($operation->getGatewayKey()), $this->settings->getDepositFlag());
-        $this->signer = new Signer(
-            $this->settings->getPrivateKey($operation->getGatewayKey()),
-            $this->settings->getPrivateKeyPassword($operation->getGatewayKey()),
-            $this->settings->getPublicKey()
-        );
+	/**
+	 * @param Operation $operation
+	 * @return $this
+	 */
+	public function createRequest(Operation $operation)
+	{
+		$this->request = new Request(
+			$operation,
+			$this->settings->getMerchantNumber($operation->getGatewayKey()),
+			$this->settings->getDepositFlag()
+		);
 
-        return $this;
-    }
+		$this->signer = new Signer(
+			$this->settings->getPrivateKey($operation->getGatewayKey()),
+			$this->settings->getPrivateKeyPassword($operation->getGatewayKey()),
+			$this->settings->getPublicKey()
+		);
 
-    /**
-     * @return Request
-     */
-    public function getRequest()
-    {
-        return $this->request;
-    }
+		return $this;
+	}
 
-    /**
-     * @return Signer
-     */
-    public function getSigner()
-    {
-        return $this->signer;
-    }
+	/**
+	 * @return Request
+	 */
+	public function getRequest()
+	{
+		return $this->request;
+	}
 
-    public function getRequestUrl()
-    {
-        $params = $this->request->getParams();
-        $this->request->setDigest($this->signer->sign($params));
-        $paymentUrl = $this->settings->getUrl() . '?' . http_build_query($this->request->getParams());
+	/**
+	 * @return Signer
+	 */
+	public function getSigner()
+	{
+		return $this->signer;
+	}
 
-        return $paymentUrl;
-    }
+	public function getRequestUrl()
+	{
+		$params = $this->request->getParams();
+		$this->request->setDigest($this->signer->sign($params));
+		$paymentUrl = $this->settings->getUrl() . '?' . http_build_query($this->request->getParams());
 
-    /**
-     * @param $params
-     * @return Response
-     */
-    public function createResponse($params)
-    {
-        $operation = isset ($params ['OPERATION']) ? $params ['OPERATION'] : '';
-        $ordernumber = isset ($params ['ORDERNUMBER']) ? $params ['ORDERNUMBER'] : '';
-        $merordernum = isset ($params ['MERORDERNUM']) ? $params ['MERORDERNUM'] : NULL;
-        $md = isset ($params ['MD']) ? $params['MD'] : NULL;
-        $prcode = isset ($params ['PRCODE']) ? $params ['PRCODE'] : '';
-        $srcode = isset ($params ['SRCODE']) ? $params ['SRCODE'] : '';
-        $resulttext = isset ($params ['RESULTTEXT']) ? $params ['RESULTTEXT'] : '';
-        $digest = isset ($params ['DIGEST']) ? $params ['DIGEST'] : '';
-        $digest1 = isset ($params ['DIGEST1']) ? $params ['DIGEST1'] : '';
+		return $paymentUrl;
+	}
 
-        $key = explode('|', $md, 2);
+	/**
+	 * @param $params
+	 * @return Response
+	 */
+	public function createResponse($params)
+	{
+		$operation = isset ($params ['OPERATION']) ? $params ['OPERATION'] : '';
+		$ordernumber = isset ($params ['ORDERNUMBER']) ? $params ['ORDERNUMBER'] : '';
+		$merordernum = isset ($params ['MERORDERNUM']) ? $params ['MERORDERNUM'] : NULL;
+		$md = isset ($params ['MD']) ? $params['MD'] : NULL;
+		$prcode = isset ($params ['PRCODE']) ? $params ['PRCODE'] : '';
+		$srcode = isset ($params ['SRCODE']) ? $params ['SRCODE'] : '';
+		$resulttext = isset ($params ['RESULTTEXT']) ? $params ['RESULTTEXT'] : '';
+		$digest = isset ($params ['DIGEST']) ? $params ['DIGEST'] : '';
+		$digest1 = isset ($params ['DIGEST1']) ? $params ['DIGEST1'] : '';
 
-        if(is_null($key[0])){
-            $gatewayKey = $this->settings->getDefaultGatewayKey();
-        } else{
-            $gatewayKey = $key[0];
-        }
-        return new Response($operation, $ordernumber, $merordernum, $md, $prcode, $srcode, $resulttext, $digest, $digest1, $gatewayKey);
-    }
+		$key = explode('|', $md, 2);
 
-    /**
-     * @param Response $response
-     * @return bool
-     * @throws GPWebPayException
-     * @throws GPWebPayResultException
-     */
-    public function verifyPaymentResponse(Response $response)
-    {
-        // verify digest & digest1
-        try {
-            $this->signer = new Signer(
-                $this->settings->getPrivateKey($response->getGatewayKey()),
-                $this->settings->getPrivateKeyPassword($response->getGatewayKey()),
-                $this->settings->getPublicKey()
-            );
+		if (is_null($key[0])) {
+			$gatewayKey = $this->settings->getDefaultGatewayKey();
+		} else {
+			$gatewayKey = $key[0];
+		}
+		return new Response($operation, $ordernumber, $merordernum, $md, $prcode, $srcode, $resulttext, $digest,
+			$digest1, $gatewayKey);
+	}
 
-            $responseParams = $response->getParams();
-            $this->signer->verify($responseParams, $response->getDigest());
-            $responseParams['MERCHANTNUMBER'] = $this->settings->getMerchantNumber($response->getGatewayKey());
-            $this->signer->verify($responseParams, $response->getDigest1());
-        } catch (SignerException $e) {
-            throw new GPWebPayException($e->getMessage(), $e->getCode(), $e);
-        }
-        // verify PRCODE and SRCODE
-        if (FALSE !== $response->hasError()) {
-            throw new GPWebPayResultException("Response has an error.", $response->getPrcode(), $response->getSrcode(), $response->getResultText());
-        }
+	/**
+	 * @param Response $response
+	 * @return bool
+	 * @throws GPWebPayException
+	 * @throws GPWebPayResultException
+	 */
+	public function verifyPaymentResponse(Response $response)
+	{
+		// verify digest & digest1
+		try {
+			$this->signer = new Signer(
+				$this->settings->getPrivateKey($response->getGatewayKey()),
+				$this->settings->getPrivateKeyPassword($response->getGatewayKey()),
+				$this->settings->getPublicKey()
+			);
 
-        return TRUE;
-    }
+			$responseParams = $response->getParams();
+			$this->signer->verify($responseParams, $response->getDigest());
+			$responseParams['MERCHANTNUMBER'] = $this->settings->getMerchantNumber($response->getGatewayKey());
+			$this->signer->verify($responseParams, $response->getDigest1());
+		} catch (SignerException $e) {
+			throw new GPWebPayException($e->getMessage(), $e->getCode(), $e);
+		}
+		// verify PRCODE and SRCODE
+		if (FALSE !== $response->hasError()) {
+			throw new GPWebPayResultException("Response has an error.", $response->getPrcode(), $response->getSrcode(),
+				$response->getResultText());
+		}
+
+		return TRUE;
+	}
 }
