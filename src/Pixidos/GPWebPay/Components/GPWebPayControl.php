b71d824035e9b3debad7ae48f8471f4c1a4ca763
Ondra Votava <ja@ondravotava.cz>
2016-06-22 14:43:00 +0200

    reformat code

--- src/Pixidos/GPWebPay/Components/GPWebPayControl.php
+++ src/Pixidos/GPWebPay/Components/GPWebPayControl.php
@@ -30,139 +30,114 @@ use Pixidos\GPWebPay\Request;
  */
 class GPWebPayControl extends UI\Control
 {
-    /**
-     * @var array of callbacks, signature: function(GPWebPayControl $control)
-     */
-    public $onCheckout = array();
-    /**
-     * @var array of callbacks, signature: function(GPWebPayControl $control, Response $response)
-     */
-    public $onSuccess = array();
-    /**
-     * @var array of callbacks, signature: function(GPWebPayControl $control, \Exception $exception)
-     */
-    public $onError = array();
-    /**
-     * @var Operation $operation
-     */
-    private $operation;
-    /**
-     * @var  Provider $provider
-     */
-    private $provider;
-    /**
-     * @var  string $templateFile
-     */
-    private $templateFile;
+	/**
+	 * @var array of callbacks, signature: function(GPWebPayControl $control)
+	 */
+	public $onCheckout = array();
+	/**
+	 * @var array of callbacks, signature: function(GPWebPayControl $control, Response $response)
+	 */
+	public $onSuccess = array();
+	/**
+	 * @var array of callbacks, signature: function(GPWebPayControl $control, \Exception $exception)
+	 */
+	public $onError = array();
+	/**
+	 * @var Operation $operation
+	 */
+	private $operation;
+	/**
+	 * @var  Provider $provider
+	 */
+	private $provider;
+	/**
+	 * @var  string $templateFile
+	 */
+	private $templateFile;
 
-    /**
-     * @param Operation $operation
-     * @param Provider $provider
-     * @param IContainer|null $control
-     * @param string $name
-     */
-    public function __construct(Operation $operation, Provider $provider, IContainer $control = null, $name = null)
-    {
-        parent::__construct($control, $name);
+	/**
+	 * @param Operation $operation
+	 * @param Provider $provider
+	 * @param IContainer|null $control
+	 * @param string $name
+	 */
+	public function __construct(Operation $operation, Provider $provider, IContainer $control = NULL, $name = NULL)
+	{
+		parent::__construct($control, $name);
 
-        $this->operation = $operation;
-        $this->provider = $provider;
-    }
+		$this->operation = $operation;
+		$this->provider = $provider;
+	}
 
-    /**
-     * @throws GPWebPayException
-     * @throws \Exception
-     */
-    public function handleCheckout()
-    {
-        try {
-            if (!$this->operation->getResponseUrl())
-                $this->operation->setResponseUrl($this->link('//success!'));
+	/**
+	 * @throws GPWebPayException
+	 * @throws \Exception
+	 */
+	public function handleCheckout()
+	{
+		try {
+			if (!$this->operation->getResponseUrl()) {
+				$this->operation->setResponseUrl($this->link('//success!'));
+			}
 
-            $url = $this->provider->createRequest($this->operation)->getRequestUrl();
-            $this->onCheckout($this, $this->provider->getRequest());
-            $this->getPresenter()->redirectUrl($url);
-        } catch (GPWebPayException $e) {
-            throw $e;
-        }
+			$url = $this->provider->createRequest($this->operation)->getRequestUrl();
+			$this->onCheckout($this, $this->provider->getRequest());
+			$this->getPresenter()->redirectUrl($url);
 
-
-    }
-
-
-
-    /**
-     * @throws GPWebPayException
-     */
-    public function handleSuccess()
-    {
-        $params = $this->getPresenter()->getParameters();
-
-        try {
-            /** @var Response $response */
-            $response = $this->provider->createResponse($params);
-            $this->provider->verifyPaymentResponse($response);
-        } catch (GPWebPayException $e) {
-            $this->errorHandler($e);
-            return;
-        }
-
-        $this->onSuccess($this, $response);
-    }
+			
+		} catch (GPWebPayException $e) {
+			throw $e;
+		}
 
 
-    /**
-     * @param GPWebPayException $exception
-     * @throws GPWebPayException
-     */
-    protected function errorHandler(GPWebPayException $exception)
-    {
-        if (! $this->onError) {
-            throw $exception;
-        }
+	}
 
-        $this->onError($this, $exception);
-    }
 
-    /**
-     * @param string $templateFile
-     */
-    public function setTemplateFile($templateFile)
-    {
-        $this->templateFile = $templateFile;
-    }
+	/**
+	 * @throws GPWebPayException
+	 */
+	public function handleSuccess()
+	{
+		$params = $this->getPresenter()->getParameters();
 
-    /**
-     * @param array $attrs
-     * @param string $text
-     */
-    public function render($attrs = array(), $text = "Pay")
-    {
-        $template = $this->template;
-        $template->setFile($this->getTemplateFilePath());
-        $template->checkoutLink = $this->link('//checkout!');
-        $template->text = $text;
-        $template->attrs = $attrs;
-        $this->template->render();
-    }
+		try {
+			/** @var Response $response */
+			$response = $this->provider->createResponse($params);
+			$this->provider->verifyPaymentResponse($response);
+		} catch (GPWebPayException $e) {
+			$this->errorHandler($e);
+			return;
+		}
 
-    /**
-     * @return string
-     */
-    public function getTemplateFilePath()
-    {
-        return ($this->templateFile)
-            ? $this->templateFile
-            : $this->getDefaultTemplateFilePath();
-    }
+		$this->onSuccess($this, $response);
+	}
 
-    /**
-     * @return string
-     */
-    public function getDefaultTemplateFilePath()
-    {
-        return __DIR__ . DIRECTORY_SEPARATOR . 'templates/gpWebPayControl.latte';
-    }
+
+	/**
+	 * @param GPWebPayException $exception
+	 * @throws GPWebPayException
+	 */
+	protected function errorHandler(GPWebPayException $exception)
+	{
+		if (!$this->onError) {
+			throw $exception;
+		}
+
+		$this->onError($this, $exception);
+	}
+
+	/**
+	 * @param string $templateFile
+	 */
+	public function setTemplateFile($templateFile)
+	{
+		$this->templateFile = $templateFile;
+	}
+
+	/**
+	 * @param array $attrs
+	 * @param string $text
+	 */
 	public function render($attrs = array(), $text = "Pay")
 	{
 		/** @var Template $template */
@@ -174,4 +149,21 @@ class GPWebPayControl extends UI\Control
 		$template->render();
 	}
 
+	/**
+	 * @return string
+	 */
+	public function getTemplateFilePath()
+	{
+		return ($this->templateFile)
+			? $this->templateFile
+			: $this->getDefaultTemplateFilePath();
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getDefaultTemplateFilePath()
+	{
+		return __DIR__ . DIRECTORY_SEPARATOR . 'templates/gpWebPayControl.latte';
+	}
 }
