da215923d9b2aa4c41202fdb9193d7a29236dc0a
Ondra Votava <ondra@votava.dev>
2021-05-16 21:18:00 +0200

    fix phpstan

---
+++ src/aliases.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+// phpcs:disable
+namespace Nette\Bridges\ApplicationLatte;
+
+if (!class_exists('Nette\Bridges\ApplicationLatte\DefaultTemplate')) {
+
+    class  DefaultTemplate extends Template
+    {
+
+    }
+}
+
+// phpcs:enable
